d515ffaabd348d6b189b7767a02154627510d561
Shehab hassan <shehabshwki@gmail.com>
2025-08-28 10:17:34 +0300

    edit file of yml to build the code

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -26,10 +26,12 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  Serial.println("Hello everyone !!!");
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
